9aaeb54a59b5e7ea0c8bde278dc0b9eb8e08c9dd
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-02-20 16:31:42 +0700

    users

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [data, setData] = useState([]);
   const handleButton = () => {
     setCounter(counter + 1);
   };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
   return (
     <>
-      <h1>value{counter}</h1>
-      <button onClick={handleButton}>increament</button>
+      <div>
+        <h1>value:{counter}</h1>
+        <button onClick={handleButton}>increament</button>
+        <hr />
+        {data.map((item) => (
+          <div key={item.id}>
+            <h1>name: {item.name}</h1>
+            <p>email: {item.email}</p>
+            <p>phone: {item.phone}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
